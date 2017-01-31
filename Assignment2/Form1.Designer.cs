9c66ddaa120d3a43ca05581c05925d8b857dd374
7059705091 <verma.sahil3232@gmail.com>
2017-01-31 02:31:44 -0500

    added GUI

--- Assignment2/Form1.Designer.cs
+++ Assignment2/Form1.Designer.cs
@@ -53,11 +53,8 @@
             this.calculate = new System.Windows.Forms.Button();
             this.clear = new System.Windows.Forms.Button();
             this.exit = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.additionalItems.SuspendLayout();
             this.exteriorFinish.SuspendLayout();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Price
@@ -311,33 +308,11 @@
             this.exit.Text = "Exit";
             this.exit.UseVisualStyleBackColor = true;
             // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(441, 23);
-            this.panel1.TabIndex = 19;
-            // 
-            // button1
-            // 
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
-            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
-            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Yellow;
-            this.button1.Location = new System.Drawing.Point(8, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // SharpAutoCenter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(441, 384);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.exit);
             this.Controls.Add(this.clear);
             this.Controls.Add(this.calculate);
@@ -365,7 +340,6 @@
             this.additionalItems.PerformLayout();
             this.exteriorFinish.ResumeLayout(false);
             this.exteriorFinish.PerformLayout();
-            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -398,8 +372,6 @@
         private System.Windows.Forms.Button calculate;
         private System.Windows.Forms.Button clear;
         private System.Windows.Forms.Button exit;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
     }
 }
 
